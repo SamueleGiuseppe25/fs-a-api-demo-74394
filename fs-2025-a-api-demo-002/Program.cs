1584fe1cec771fe0c1ca9792a8cd485260f56ac2
sakshi <sakshi.panchale@dorset.ie>
2025-09-25 09:49:15 +0100

    added in models

--- fs-2025-a-api-demo-002/Program.cs
+++ fs-2025-a-api-demo-002/Program.cs
@@ -29,7 +29,4 @@ app.AddCourseEndPoints();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
