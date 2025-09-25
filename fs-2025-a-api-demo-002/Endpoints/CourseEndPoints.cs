9b844ff90cb463a0611b88c4001ac58fba1c6750
sakshi <sakshi.panchale@dorset.ie>
2025-09-25 09:37:55 +0100

    Add project files.

---
+++ fs-2025-a-api-demo-002/Endpoints/CourseEndPoints.cs
@@ -0,0 +1,10 @@
+﻿namespace fs_2025_a_api_demo_002.Endpoints
+{
+    public static class CourseEndPoints
+    {
+        public static void AddCourseEndPoints(this WebApplication app)
+        {
+
+        }
+    }
+}
---
+++ fs-2025-a-api-demo-002/Endpoints/WeatherEndPoints.cs
@@ -0,0 +1,10 @@
+﻿namespace fs_2025_a_api_demo_002.Endpoints
+{
+    public static class WeatherEndPoints
+    {
+        public static void AddWeatherEndPoints(this WebApplication app)
+        {
+
+        }
+    }
+}
