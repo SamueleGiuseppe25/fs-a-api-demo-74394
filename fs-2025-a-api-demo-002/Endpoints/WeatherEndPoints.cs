12b890058d66b1e3bd1bf48233b5bc11d80cf368
sakshi <sakshi.panchale@dorset.ie>
2025-09-25 09:45:29 +0100

    Added weather endpoint

--- fs-2025-a-api-demo-002/Endpoints/WeatherEndPoints.cs
+++ fs-2025-a-api-demo-002/Endpoints/WeatherEndPoints.cs
@@ -2,9 +2,26 @@
 {
     public static class WeatherEndPoints
     {
+        static string[] summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
         public static void AddWeatherEndPoints(this WebApplication app)
         {
-
+            app.MapGet("/weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                    .ToArray();
+                return forecast;
+            })
+.WithName("GetWeatherForecast")
+.WithOpenApi();
         }
     }
 }
